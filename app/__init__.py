18f23ad9d7a6ef2d9b24443c0e70ca4b7b6db220
Omar Rodrigues <161538673+omariscode@users.noreply.github.com>
2024-12-06 09:09:45 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
+import os
 import firebase_admin
 from flask import Flask
 from flask_cors import CORS
+from huggingface_hub import InferenceClient
 from firebase_admin import credentials, firestore
+
+IMAGE_DIR = "static/images"
+os.makedirs(IMAGE_DIR, exist_ok=True)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hhahaha'
@@ -12,5 +17,7 @@ cred = credentials.Certificate("./story.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+client = InferenceClient("black-forest-labs/FLUX.1-dev", token="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")
 
 from app import routes
